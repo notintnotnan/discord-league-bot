57ad78a1ea10f33389c0c91b383a47c2c29fbe86
Oscar Ramirez <oscarframirezp@hotmail.com>
2025-11-18 17:33:02 -0500

    Added setup for phrases tabke and its contents.

--- modules/setup.py
+++ modules/setup.py
@@ -16,26 +16,66 @@ try:
 except Exception as e:
     print(f"Error connecting to database: {e}")
 
-query = '''
-CREATE TABLE DISCORD_BOT_LEAGUE(
-    PUUID varchar(78) PRIMARY KEY,
-    CREATED_AT date NOT NULL,
-    DELETED_AT date, 
-    FLEX_DIVISION varchar(20),
-    FLEX_RANK int,
-    SOLO_DIVISION varchar(20),
-    SOLO_RANK int
-)
-'''
-
 try:
     cur = conn.cursor()
-    cur.execute(query)
+    cur.execute('''
+    CREATE TABLE DISCORD_BOT_LEAGUE(
+        PUUID varchar(78) PRIMARY KEY,
+        CREATED_AT date NOT NULL,
+        DELETED_AT date, 
+        FLEX_DIVISION varchar(20),
+        FLEX_RANK int,
+        FLEX_POINTS int,
+        SOLO_DIVISION varchar(20),
+        SOLO_RANK int,
+        SOLO_POINTS int,
+        UPDATED_AT timestamp
+    )
+    '''
+    )
     conn.commit()
-    cur.close()
-    conn.close()
+    cur.execute('''
+    CREATE TABLE DISCORD_BOT_LEAGUE_PHRASES(
+        ID SERIAL PRIMARY KEY,
+        SENTIMENT boolean NOT NULL,
+        PHRASE varchar NOT NULL
+    )
+    ''')
+    conn.commit()
 except Exception as e:
     if cur:
         cur.close()
     conn.close()
     print(f"Error encountered while creating the table: {e}")
+
+try:
+    cur.execute('''
+    CREATE TABLE DISCORD_BOT_LEAGUE_PHRASES(
+        ID SERIAL PRIMARY KEY,
+        SENTIMENT boolean NOT NULL,
+        PHRASE varchar NOT NULL
+    )
+    ''')
+    conn.commit()
+    with open('../media/phrases.txt', encoding='utf-8') as phrases:
+        for line in phrases.readlines():
+            sentiment, phrase = line.split(';')
+            cur.execute('''
+            INSERT INTO DISCORD_BOT_LEAGUE_PHRASES (
+                SENTIMENT,
+                PHRASE            
+            ) VALUES (
+                %s,
+                %s
+            )
+            ''',(sentiment, phrase))
+            conn.commit()
+    
+except Exception as e:
+    cur.close()
+    conn.close()
+    print(f"Error encountered while creating the table: {e}")
+
+if cur:
+    cur.close()
+    conn.close()
